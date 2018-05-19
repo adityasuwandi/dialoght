fad7c1f07d0a741ab721dcbf3c489f02177ffed9
adityasuwandi <markusaditya01@gmail.com>
2018-05-19 18:28:32 +0700

    lalalala

--- app.py
+++ app.py
@@ -46,8 +46,26 @@ def webhook():
     # print(res)
     r = make_response(res)
     r.headers['Content-Type'] = 'application/json'
+    return r
+
+
+def processRequest(req):
+    res = makeWebhookResult()
+    return res
+
+
+
+def makeWebhookResult():
+
+    # print(json.dumps(item, indent=4))
+
+    speech = "Cuaca sekarang di"
+
+    print("Response:")
+    print(speech)
+
     return {
-    "fulfillmentText": "This is a text response",
+        "fulfillmentText": "This is a text response",
         "fulfillmentMessages": [
           {
             "card": {
@@ -63,70 +81,6 @@ def webhook():
             }
           }
         ]
-    }
-
-
-def processRequest(req):
-    if req.get("result").get("action") != "yahooWeatherForecast":
-        return {}
-    baseurl = "https://query.yahooapis.com/v1/public/yql?"
-    yql_query = makeYqlQuery(req)
-    if yql_query is None:
-        return {}
-    yql_url = baseurl + urlencode({'q': yql_query}) + "&format=json"
-    result = urlopen(yql_url).read()
-    data = json.loads(result)
-    res = makeWebhookResult(data)
-    return res
-
-
-def makeYqlQuery(req):
-    result = req.get("result")
-    parameters = result.get("parameters")
-    city = parameters.get("geo-city")
-    if city is None:
-        return None
-
-    return "select * from weather.forecast where woeid in (select woeid from geo.places(1) where text='" + city + "')"
-
-
-def makeWebhookResult(data):
-    query = data.get('query')
-    if query is None:
-        return {}
-
-    result = query.get('results')
-    if result is None:
-        return {}
-
-    channel = result.get('channel')
-    if channel is None:
-        return {}
-
-    item = channel.get('item')
-    location = channel.get('location')
-    units = channel.get('units')
-    if (location is None) or (item is None) or (units is None):
-        return {}
-
-    condition = item.get('condition')
-    if condition is None:
-        return {}
-
-    # print(json.dumps(item, indent=4))
-
-    speech = "Cuaca sekarang di " + location.get('city') + ": " + condition.get('text') + \
-             ", Dan suhunya sekitar " + condition.get('temp') + " " + units.get('temperature')
-
-    print("Response:")
-    print(speech)
-
-    return {
-        "speech": speech,
-        "displayText": speech,
-        # "data": data,
-        # "contextOut": [],
-        "source": "apiai-weather-webhook-sample"
     }
 
 
