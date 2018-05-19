74446c3f202da28c5ca4f258092480e1e6103bf7
adityasuwandi <markusaditya01@gmail.com>
2018-05-19 15:13:05 +0700

    lalalala

--- app.py
+++ app.py
@@ -50,16 +50,19 @@ def webhook():
 
 
 def processRequest(req):
-    if req.get("result").get("action") != "yahooWeatherForecast":
-        return {}
-    baseurl = "https://query.yahooapis.com/v1/public/yql?"
-    yql_query = makeYqlQuery(req)
-    if yql_query is None:
-        return {}
-    yql_url = baseurl + urlencode({'q': yql_query}) + "&format=json"
-    result = urlopen(yql_url).read()
-    data = json.loads(result)
-    res = makeWebhookResult(data)
+    # if req.get("result").get("action") != "yahooWeatherForecast":
+    #     return {}
+    # baseurl = "https://query.yahooapis.com/v1/public/yql?"
+    # yql_query = makeYqlQuery(req)
+    # if yql_query is None:
+    #     return {}
+    # yql_url = baseurl + urlencode({'q': yql_query}) + "&format=json"
+    # result = urlopen(yql_url).read()
+    # data = json.loads(result)
+    result = req.get("result")
+    parameters = result.get("parameters")
+    city = parameters.get("geo-city")
+    res = makeWebhookResult(city)
     return res
 
 
@@ -73,33 +76,11 @@ def makeYqlQuery(req):
     return "select * from weather.forecast where woeid in (select woeid from geo.places(1) where text='" + city + "')"
 
 
-def makeWebhookResult(data):
-    query = data.get('query')
-    if query is None:
-        return {}
-
-    result = query.get('results')
-    if result is None:
-        return {}
-
-    channel = result.get('channel')
-    if channel is None:
-        return {}
-
-    item = channel.get('item')
-    location = channel.get('location')
-    units = channel.get('units')
-    if (location is None) or (item is None) or (units is None):
-        return {}
-
-    condition = item.get('condition')
-    if condition is None:
-        return {}
+def makeWebhookResult(city):
 
     # print(json.dumps(item, indent=4))
 
-    speech = "Cuaca sekarang di " + location.get('city') + ": " + condition.get('text') + \
-             ", Dan suhunya sekitar " + condition.get('temp') + " " + units.get('temperature')
+    speech = "Cuaca sekarang di " + city + ": "
 
     print("Response:")
     print(speech)
